7a4fffdf3fe8abb7edeab96cdb7e988ba87cf3b3
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-02-26 12:28:55 -0300

    0.1.2.1

--- lib/pages/store.dart
+++ lib/pages/store.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:protify/components/connection.dart';
 
@@ -10,19 +14,74 @@ class StorePage extends StatefulWidget {
 
 class _StorePageState extends State<StorePage> {
   bool loaded = false;
+  Map<int, Map?> mainGames = {};
+
+  Future<Map<int, Map?>> getMainGames() async {
+    //Communicate with the server
+    final response = await Connection.sendMessage(context, address: "/store_main", requestType: "GET");
+    //Check for errors
+    if (Connection.errorTreatment(context, response)) {
+      //Swipe every games and add to the main games variable
+      final games = jsonDecode(response.body)["content"]["games"];
+      for (int i = 0; i < games.length; i++) {
+        mainGames[games[i]] = null;
+      }
+    }
+    return mainGames;
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (!loaded) {
-      loaded = true;
-      Connection.sendMessage(context, address: "/store_main", requestType: "GET").then((response) {
-        if (Connection.errorTreatment(context, response)) {
-          //To do
-        }
-      });
-    }
-    return const Scaffold(
-      body: Text("This feature is not implemented yet"),
+    return Scaffold(
+      body: Column(
+        children: [
+          //Top bar
+          Row(
+            children: [
+              IconButton(
+                //Back to home screen
+                onPressed: () => Navigator.pushNamedAndRemoveUntil(context, 'home', (route) => false),
+                //Back icon
+                icon: const Icon(Icons.arrow_back),
+              ),
+            ],
+          ),
+          FutureBuilder(
+            future: getMainGames(),
+            builder: (context, future) {
+              return future.hasData
+                  ? GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        crossAxisSpacing: 0,
+                        childAspectRatio: 0.5,
+                      ),
+                      shrinkWrap: true,
+                      itemCount: mainGames.length,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () => {},
+                          child: Container(
+                            color: Theme.of(context).primaryColor,
+                            child: Center(
+                              child: Text(
+                                mainGames[index] == null ? "Loading..." : mainGames[index]!["Title"],
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.secondary,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  : const CircularProgressIndicator();
+            },
+          )
+        ],
+      ),
     );
   }
 }
